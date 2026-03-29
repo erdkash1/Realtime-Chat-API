4d661760e267720b73d42b9ae643787324e2400e
Erdkash1 <erdkash1@gmail.com>
2026-03-28 23:03:41 -0500

    initial setup: pom.xml and application.properties

---
+++ src/main/java/com/iggy/realtimechat/RealTimeChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.iggy.realtimechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeChatApiApplication.class, args);
+    }
+
+}
