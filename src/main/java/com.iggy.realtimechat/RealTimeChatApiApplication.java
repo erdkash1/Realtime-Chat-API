b3e7f95213c323704bdfb3637ff5f199d6765ed1
Erdkash1 <erdkash1@gmail.com>
2026-04-02 23:23:39 -0500

    fix: untrack pom.xml from gitignore

--- src/main/java/com.iggy.realtimechat/RealTimeChatApiApplication.java
+++ src/main/java/com.iggy.realtimechat/RealTimeChatApiApplication.java
@@ -2,8 +2,10 @@ package com.iggy.realtimechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
+})
 
-@SpringBootApplication
 public class RealTimeChatApiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com.iggy.realtimechat/entity/Message.java
@@ -0,0 +1,40 @@
+package com.iggy.realtimechat.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @CreationTimestamp
+    @Column(name = "sent_at", updatable = false)
+    private LocalDateTime sentAt;
+
+
+}
